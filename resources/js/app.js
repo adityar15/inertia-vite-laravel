426ebfbf8cefb0462e7d5396467d453476d26929
adityar15 <adityask2194@gmail.com>
2022-07-09 15:57:54 +0100

    inertia layout

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,14 @@
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/inertia-vue3'
 // import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
-
+import Layout from './components/Layout.vue'
 
 createInertiaApp({
-  resolve: async (name) => {return (await import(`./Pages/${name}.vue`)).default},
+  resolve: async (name) => {
+    const page = (await import(`./Pages/${name}.vue`)).default
+    page.layout = page.layout || Layout
+    return page
+  },
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
       .use(plugin)
---
+++ resources/js/components/Layout.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="space-y-4">
+        <div class="h-16 bg-sky-600 p-1">
+        </div>
+        <slot />
+    </div>
+</template>
